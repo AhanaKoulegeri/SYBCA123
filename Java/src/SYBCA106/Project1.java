4953ed0bd1e90ca363c8d1fec715aa820ace3c65
AhanaKoulegeri <koulegeriahana@gmail.com>
2024-01-10 11:07:12 +0530

    Commit

---
+++ Java/src/SYBCA106/Project1.java
@@ -0,0 +1,14 @@
+package SYBCA106;
+
+public class Project1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		System.out.println("Hello");
+
+
+	}
+
+}
